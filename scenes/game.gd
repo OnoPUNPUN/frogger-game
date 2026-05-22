e1622e2e6cad54a448503041c088ec6e8e5a6135
OnoPUNPUN <punpunonodera754@gmail.com>
2026-05-23 01:41:48 +0600

    add Car Randomizer

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-
-
-
-
+var car_scene: PackedScene = preload("res://scenes/car.tscn")
 
 func _on_finish_area_body_entered(body: Node2D) -> void:
 	print(body)
 	print("has entered")
+
+
+func _on_car_timer_timeout() -> void:
+	var car = car_scene.instantiate() as Area2D
+	var pos_marker = $CarStartPosition.get_children().pick_random() as Marker2D
+	car.position = pos_marker.position
+	$Objects.add_child(car)
+	car.connect("body_entered", go_to_title)
+	
+func go_to_title(body):
+	print(body)
+	print("player car Hit")
